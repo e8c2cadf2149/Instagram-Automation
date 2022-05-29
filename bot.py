9c06b8f70d7e60b4ae02304eb079b3e8d1fb3d8a
gautambisht709 <101648317+e8c2cadf2149@users.noreply.github.com>
2022-05-29 11:36:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+
+                 
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
@@ -58,6 +60,11 @@ def getRandomTime():
         randTime = randint(3, 5)
         return randTime      
 InstaBot("your_usernmae", "your_password")   
+
+
+
+
+
 
 
 
