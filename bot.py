73313b2327ad5deb3fab435fa098f70013ba3226
gautambisht709 <101648317+e8c2cadf2149@users.noreply.github.com>
2022-05-29 10:47:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,9 +19,9 @@ class InstaBot:
         driver.implicitly_wait(30)  
         time.sleep(getRandomTime())
         driver.find_element_by_xpath("//input[@name=\"username\"]")\
-            .send_keys('Your_Usename')
+            .send_keys('automation.11')
         driver.find_element_by_xpath("//input[@name=\"password\"]")\
-            .send_keys('Your_password')
+            .send_keys('e8c2cadf2147')
         time.sleep(getRandomTime())      
         driver.find_element_by_xpath('//button[@type="submit"]')\
             .click()
@@ -45,7 +45,7 @@ class InstaBot:
                   driver.implicitly_wait(30)
                   follow = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/div/div/div/span/span[1]/button').click()
                   print('Following')
-                  time.sleep(600)
+                  time.sleep(1210)
                   unfollow1 = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/div/div[2]/div/span/span[1]/button').click()
                   time.sleep(2)
                   unfollow2 = driver.find_element_by_xpath('/html/body/div[6]/div/div/div/div[3]/button[1]').click()
