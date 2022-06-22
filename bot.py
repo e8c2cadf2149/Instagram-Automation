ad80fae38aff0f9969f29d613cda1b0ddf0c54e5
gautambisht709 <101648317+e8c2cadf2149@users.noreply.github.com>
2022-06-22 08:01:09 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,12 +7,16 @@ import os
 from random import seed
 from random import randint
 import random
+
+
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(ChromeDriverManager().install())
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+#driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 def getRandomTime():
         randTime = randint(5, 10)
