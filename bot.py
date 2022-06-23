f78bd21934db60acd17fafaa10c8dc2e4c1fb28b
gautambisht709 <101648317+e8c2cadf2149@users.noreply.github.com>
2022-06-23 00:40:53 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -65,7 +65,7 @@ def follow1():
           #     else:
           #         button[0].click()
           #         time.sleep(2)
-    time.sleeep(1210)      #         driver.find_element_by_xpath("/html/body/div[5]/div/div/div/div[3]/button[1]").click()      
+    time.sleep(1210)      #         driver.find_element_by_xpath("/html/body/div[5]/div/div/div/div[3]/button[1]").click()      
     driver.implicitly_wait(30)
     button = driver.find_element_by_xpath("//button[@class='_abn9 _abnd _abni _abnn']")
     button.click()
